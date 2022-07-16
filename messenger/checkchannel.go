0607528fb6a2d611fe744d4d5fe57a29474cf29d
Michael Curtis <mpbcurtis@gmail.com>
2022-07-16 19:35:19 -0400

    environment webhook url

--- messenger/checkchannel.go
+++ messenger/checkchannel.go
@@ -10,8 +10,9 @@ import (
 func (c ChannelType) checkChannelType() (string, error) {
 	url := c.Channel
 	env := os.Getenv("ENVIRONMENT")
+	var channelURL string
 	if env == "development" || config.AppConfig.Application.Environment == "development" {
-		var channelURL string
+
 		if c.ChannelType == "discord" {
 			channelURL = os.Getenv("DEV_DISCORD_CHANNEL_WEBHOOK_URL")
 		} else {
@@ -20,7 +21,6 @@ func (c ChannelType) checkChannelType() (string, error) {
 		url = channelURL
 	}
 	if env == "staging" || config.AppConfig.Application.Environment == "development" {
-		var channelURL string
 		if c.ChannelType == "discord" {
 			channelURL = os.Getenv("STAGING_DISCORD_CHANNEL_WEBHOOK_URL")
 		} else {
@@ -29,7 +29,6 @@ func (c ChannelType) checkChannelType() (string, error) {
 		url = channelURL
 	}
 	if env == "production" || config.AppConfig.Application.Environment == "development" {
-		var channelURL string
 		if c.ChannelType == "discord" {
 			channelURL = os.Getenv("PRODUCTION_DISCORD_CHANNEL_WEBHOOK_URL")
 		} else {
@@ -38,7 +37,12 @@ func (c ChannelType) checkChannelType() (string, error) {
 		url = channelURL
 	}
 	if url == "" {
-		return "", errors.New("no webhook found")
+		defaultURL := os.Getenv("DEFAULT_CHANNEL_WEBHOOK_URL")
+		if len(defaultURL) != 0 {
+			url = defaultURL
+		} else {
+			return "", errors.New("no webhook found")
+		}
 	}
 	return url, nil
 }
