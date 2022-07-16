52d01946cec738e7af1a68c56ca094e6be462722
Michael Curtis <mpbcurtis@gmail.com>
2022-07-16 01:57:44 -0400

    language errors

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Package cscodereviewbot is a simple messenger application
+Package botty is a simple messenger application
 that posts open pull requests in a  channel.
 
 For a full guide visit https://github.com/mcereal/botty
