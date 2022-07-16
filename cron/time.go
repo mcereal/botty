bbd20956a27d3f64d705fe7de1b9cfa5f16747f7
Michael Curtis <mpbcurtis@gmail.com>
2022-07-16 14:36:34 -0400

    code smells

--- cron/time.go
+++ cron/time.go
@@ -13,10 +13,10 @@ import (
 func ElapsedTime(s time.Time, d int) (bool, string) {
 	elapsed := time.Since(s)
 
-	_duration := time.Duration(d)
-	if elapsed > _duration {
+	duration := time.Duration(d)
+	if elapsed > duration {
 		elapsedTime := elapsed.Round(time.Second)
-		exceedLimit := fmt.Sprintf("Elapsed time of %v has exceeded the time limit of %v. Please convert to draft, or approve and merge", elapsedTime, _duration)
+		exceedLimit := fmt.Sprintf("Elapsed time of %v has exceeded the time limit of %v. Please convert to draft, or approve and merge", elapsedTime, duration)
 		return true, exceedLimit
 	}
 	return false, "Warning not detected"
