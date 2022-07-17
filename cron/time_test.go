e7abb6e366f7010326f0b2e2b47f6c02dfbd15d5
Michael Curtis <mpbcurtis@gmail.com>
2022-07-16 21:19:58 -0400

    fix test

--- cron/time_test.go
+++ cron/time_test.go
@@ -3,8 +3,6 @@ package cron
 import (
 	"testing"
 	"time"
-
-	"github.com/mcereal/botty/config"
 )
 
 // TestElapsedTime checks the amount of time it has been since a PR was opned and returns true if the
@@ -12,16 +10,16 @@ import (
 func TestElapsedTime(t *testing.T) {
 	currentTime := time.Now()
 
-	for _, v := range config.AppConfig.Team {
-		count := (v.CronElapsedDuration / 60000000000) - 1
-		notElapsed := currentTime.Add(time.Duration(-count) * time.Minute)
+	duration := 14400000000000
+	count := (duration / 60000000000) - 1
+	notElapsed := currentTime.Add(time.Duration(-count) * time.Minute)
 
-		elapsed, message := ElapsedTime(notElapsed, v.CronElapsedDuration)
-		if elapsed != true {
-			t.Errorf("got %v", elapsed)
-		}
-		if message != "Warning not detected" {
-			t.Errorf("got %v", message)
-		}
+	elapsed, message := ElapsedTime(notElapsed, duration)
+	if elapsed {
+		t.Errorf("got %v and duration is %v", elapsed, count)
 	}
+	if message != "Warning not detected" {
+		t.Errorf("got %v", message)
+	}
+
 }
